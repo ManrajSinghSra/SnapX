97d52b9b2f23d869ea2ac5768fa17eb0522851d3
ManrajSingh <169031983+ManrajSinghSra@users.noreply.github.com>
2024-09-22 22:38:15 +0530

    Add files via upload

---
+++ j.js
@@ -0,0 +1,9 @@
+let section=document.querySelector('section');
+section.addEventListener('mouseover',(e)=>{
+     if(e.target.classList.contains('images_slider')){
+         console.log("ujwd")
+     }
+})
+
+
+ z
